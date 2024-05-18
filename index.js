ad70c67f3549418ce8882a841701bebfa34dd14c
macd <143769838+sabhanan@users.noreply.github.com>
2024-05-18 08:32:13 +0700

    notip

---
+++ index.js
@@ -0,0 +1,23 @@
+alert("SELAMAT DATANG DI JAVASKRIPT!")
+        confirm("apakah anda siap belajar?")
+        var name=prompt("saiapa nama anda?","")
+        document.write("helo,"+nama+"<br>")
+
+        document.write("<h1>DATA DIRI</h1><br>")
+            document.write("<b>nama lengkap</b>")
+            var namaDepan = "Muhammad"
+            var namaTengah = "Sabhanan"
+            var namaBelakang = "Naja"
+            document.write("h" + namaDepan + " " + namaBelakang + " " + namaBelakang + "<br>")
+
+            document.write('<h1>DATA DIRI</h1>')
+            var namaLengkap = 'muhammad sabhanan naja'
+            var alamat = 'mranggen, tegalarum'
+            var usia = '15 tahun'
+            var jenisKelamin = 'laki-laki'
+            var anakKe  = 'pertama'
+            document.write('nama lengkap: ' + ' ' + namaLengkap + '<br>') 
+            document.write('alamat: ' + ' ' + alamat + '<br>')
+            documen.write('usia: ' + ' ' + usia + '<br>')
+            document.write('jenis kelamin: ' + ' ' + jenisKelamin + '<br>')
+            documen.write('anak ke :' + ' ' + anakKe + '<br>')
